f5575c87d53d697364123d47c81693e70a1ac39b
주은희 <eunhee.ju@jh-solution.net>
2025-06-02 13:48:04 +0900

    [WIP]

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from '@suwon-web-front/ui';
-import Notices from '@/features/notice/components/Notices';
 
 export default function Home() {
   return (
     <div>
       <Button>Hello world</Button>
-      <Notices />
     </div>
   );
 }
